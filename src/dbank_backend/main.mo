f03eb55e7ab820fe183b67e4b4d86284af7ea68c
reedpogi <reed@reedpogi.com>
2024-04-19 18:59:03 +0800

    first commit

--- src/dbank_backend/main.mo
+++ src/dbank_backend/main.mo
@@ -1,5 +1,48 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+
+actor DBank {
+  stable var currentValue: Float = 300;
+  // currentValue := 300;
+  stable var startTime = Time.now();
+  // startTime := Time.now();
+
+  // let constant = 1231342;
+
+  Debug.print(debug_show(currentValue));
+  // Debug.print(debug_show(constant));
+
+  public func topUp(amount: Float) {
+    currentValue += amount;
+    // Debug.print(debug_show(currentValue));
   };
-};
+
+
+  public func withdraw(amount: Float) {
+    let tempValue: Float = currentValue - amount;
+
+    if (tempValue >= 0) {
+      currentValue -= amount;
+      // Debug.print(debug_show(currentValue));
+    } else {
+      Debug.print("Error on operation: tempValue is below 0.");
+    }
+  };
+
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+  // get currentValue with interest
+  public func totalValue() {
+    let currentTime = Time.now();
+    let timeNS = currentTime - startTime;
+    let timeSec = timeNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeSec));
+    startTime := currentTime;
+  }
+
+  // topUp();
+}
--- src/dbank_frontend/src/main.js
+++ src/dbank_frontend/src/main.js
@@ -1,4 +1,30 @@
-import App from './App';
-import './index.scss';
+import "./index.scss";
+import { dbank_backend } from "../../declarations/dbank_backend";
 
-const app = new App();
+window.addEventListener("load", async function () {
+  const currentAmount = await dbank_backend.checkBalance();
+  document.getElementById("value").innerText = currentAmount.toFixed(2);
+});
+
+document.querySelector("form").addEventListener("submit", async function (event) {
+  event.preventDefault();
+  document.getElementById("submit-btn").setAttribute("disabled", true);
+  const addedAmount = parseFloat(document.getElementById("input-amount").value);
+  const withdrawnAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  if (document.getElementById("input-amount").value.length != 0) {
+    await dbank_backend.topUp(addedAmount);
+  }
+  if (document.getElementById("withdrawal-amount").value.length != 0) {
+    await dbank_backend.withdraw(withdrawnAmount);
+  }
+
+  await dbank_backend.totalValue();
+
+  const currentAmount = await dbank_backend.checkBalance();
+  document.getElementById("value").innerText = currentAmount.toFixed(2);
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+  document.getElementById("submit-btn").removeAttribute("disabled");
+});
